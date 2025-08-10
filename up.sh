e56f6a5b13c5c081a684fb1f776fb3ee53c41020
Pedro Teodoro <pedro_teodoro@hotmail.com>
2025-08-10 20:00:24 +0100

    New update

--- up.sh
+++ up.sh
@@ -15,38 +15,48 @@ set -eo pipefail
 #tput setaf 8 = light blue
 ##################################################################################################################
 
-# variables and functions
+tput setaf 6
+echo "=============================================================="
+echo "                    Updating $(basename $(pwd))"
+echo "=============================================================="
+tput sgr0
+
+message="New update"
+
+while getopts ":m:" opt; do
+  case ${opt} in
+    m )
+      if [[ -n "$OPTARG" ]]; then
+        message="$OPTARG"
+      fi
+      ;;
+    \? )
+      echo "Usage: $0 [-m <message>]"
+      exit 1
+      ;;
+  esac
+done
+
+echo "====> Update the repo <===="
+
 workdir=$(pwd)
-#dir="calamares-3.3.14-01"
-#source=" /home/pedro/Programing/ArchISOs/KaosArch/kaosarch-pkgbuild/"
-#destiny="/home/pedro/Programing/ArchISOs/KaosArch/kaosarch-calamares-config/etc/calamares/pkgbuild/"
-#
-###################################################################################################################
-#
-#rm -r $destiny*
-#cp -r $source$dir/* $destiny
 
 cd x86_64
 sh updaterepo.sh
 
 cd $workdir
 
+echo "====> git update <===="
+
+git add --all .                           # Add every change to be pushed to git
+git commit -m "$message"                  # Commit to local repository with a default message
+branch=$(git rev-parse --abbrev-ref HEAD) # Get branch name origin or master
+git push -u origin "$branch"              # Push the local files to github
 # Below command will backup everything inside the project folder
-git add --all .
 
-# Committing to the local repository with a message containing the time details and commit text
-
-git commit -m "update"
-
-# Push the local files to github
-
-branch=$(git rev-parse --abbrev-ref HEAD)
-git push -u origin "$branch"
-
-echo
 tput setaf 6
-echo "##############################################################"
-echo "###################  $(basename $0) done"
-echo "##############################################################"
+echo "=============================================================="
+echo "                    $(basename $0) done"
+echo "=============================================================="
 tput sgr0
 echo
